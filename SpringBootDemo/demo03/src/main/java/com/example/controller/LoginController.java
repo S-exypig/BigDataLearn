467b5d849e38450baf8c920320d42953898ad3bb
S-exypig <1321987303@qq.com>
2023-07-13 16:11:44 +0800

    2023/7/13 集成了thymeleaf框架，构建了第一个controller和templates和配置了yml

---
+++ SpringBootDemo/demo03/src/main/java/com/example/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Calendar;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String toLoginPage(Model model){
+        model.addAttribute("currentYear",Calendar.getInstance().get(Calendar.YEAR));
+        return "login";
+    }
+}
