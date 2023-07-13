bcdaf57000ba40975e8b1e38bf6566c2625594c0
S-exypig <1321987303@qq.com>
2023-07-13 16:30:26 +0800

    2023/7/13 添加了一些注释

--- SpringBootDemo/demo03/src/main/java/com/example/controller/LoginController.java
+++ SpringBootDemo/demo03/src/main/java/com/example/controller/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.xml.transform.Templates;
 import java.util.Calendar;
 
 @Controller
@@ -11,6 +12,7 @@ public class LoginController {
     @GetMapping("/login")
     public String toLoginPage(Model model){
         model.addAttribute("currentYear",Calendar.getInstance().get(Calendar.YEAR));
+//        转发到templates页面:前缀+字符串+后缀(templates+login+.html),注意不能@RespondBody
         return "login";
     }
 }
